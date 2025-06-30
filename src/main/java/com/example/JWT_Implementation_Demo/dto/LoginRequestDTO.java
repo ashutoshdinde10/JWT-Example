1d0cafea216b8ffc340bfcca26e639e608bee234
Ashutosh Dinde <ashutosh.dinde@coditas.com>
2025-06-30 10:01:44 +0530

    sss

--- src/main/java/com/example/JWT_Implementation_Demo/dto/LoginRequestDTO.java
+++ src/main/java/com/example/JWT_Implementation_Demo/dto/LoginRequestDTO.java
@@ -3,7 +3,7 @@ package com.example.JWT_Implementation_Demo.dto;
 
 import com.example.JWT_Implementation_Demo.util.ValidDomain;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+/// ////////////////////
 public class LoginRequestDTO {
     @ValidDomain(allowedDomains = {"coditas.com", "coditas.org"}, message = "Email Not Valid")
     private String userEmail;
